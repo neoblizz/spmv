dbbc794c394973d1b54ad6b8a74c806f8ccd8c16
Jonathan Wapman <jdwapman@ucdavis.edu>
2021-02-01 21:15:02 -0800

    Update mosama's test script for SPMV on the owensgroup cluster

--- spmv_test.sh
+++ spmv_test.sh
@@ -1,11 +1,19 @@
 # Setup Paths for binary and datasets
-BIN=./bin/spmm
-PATH=/home/mosama/alt-mosama/suitesparse
-DATASET=../../datasets
-SUITES=$DATASET/suitesparse.txt
+BIN=./tests/spmv/bin/spmv
+DATASET=/data/suitesparse_dataset/MM
+SUITES=$DATASET/suitesparse_mtx.txt
+
+
+find $DATASET -type f -name "*.mtx" > datasets.txt
+
+while IFS= read -r line; do
+  echo "$line"
+  $BIN $line
+done < datasets.txt
+
 # Perform evaluation
-echo "dataset,nnz,elapsed" >> results_suite.csv
-while IFS= read -r line
-do
-  echo $BIN $PATH/$line/$line.mtx
-  $BIN $PATH/$line/
+# echo "dataset,nnz,elapsed" >> results_suite.csv
+# while IFS= read -r line
+# do
+#   echo $BIN $PATH/$line/$line.mtx
+#   $BIN $PATH/$line/
