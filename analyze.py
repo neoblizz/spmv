ef5a445c3e1a38b9340411965871c55302c18053
Jonathan Wapman <jdwapman@ucdavis.edu>
2021-02-25 20:47:51 -0800

    Add matrix data to output csv file

--- analyze.py
+++ analyze.py
@@ -7,6 +7,15 @@ outfile = "parsed_results.csv"
 suitesparse_data = pd.DataFrame()
 column_names = ""
 
+def strip_path(filepath):
+  base=os.path.basename(filepath)
+  return os.path.splitext(base)[0]
+
+# Read in the matrix data
+mtxdata = pd.read_csv("results.csv")
+mtxdata["File"] = mtxdata["File"].apply(strip_path)
+print(mtxdata)
+
 for filename_short in os.listdir(datadir):
   filename_long = datadir + filename_short
 
@@ -18,21 +27,22 @@ for filename_short in os.listdir(datadir):
 
   # Get the file data and ignore the 
   filedata = pd.read_csv(filename_long, skiprows=5)
-  # print(filedata)
 
   # Building the column headers for the first time
   if suitesparse_data.empty:
     column_names = filedata["Metric Name"].tolist()
-    column_names = ["dataset"] + column_names
+    column_names = ["dataset", "rows", "cols", "nnz", "cuSparse Runtime"] + column_names
     suitesparse_data = pd.DataFrame(columns=column_names)
 
   # Create a new row in the dataframe
   # Not necessarily the most efficient way to do this. Optimize for future speed
-  dataset = [os.path.splitext(filename_short)[0]]
+  dataset = os.path.splitext(filename_short)[0]
+
+  mtx_stats = mtxdata.loc[mtxdata["File"]==dataset].values.tolist()[0][1:]
+
   metrics = filedata["Avg"].tolist()
-  new_row = dataset + metrics
+  new_row = [dataset] + mtx_stats + metrics
   suitesparse_data.loc[len(suitesparse_data)] = new_row 
-  # suitesparse_data.append(new_row)
 
 print(suitesparse_data)
 suitesparse_data.to_csv(outfile)
