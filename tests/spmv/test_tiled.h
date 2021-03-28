29d7e1ffb5026e1e2a69c14fb6384fc60d4f22f5
Jonathan Wapman <jdwapman@ucdavis.edu>
2021-03-28 08:11:51 -0700

    Allocate all shmem per block

--- tests/spmv/test_tiled.h
+++ tests/spmv/test_tiled.h
@@ -9,18 +9,16 @@ namespace cg = cooperative_groups;
 
 template <typename index_t = int, typename value_t = float>
 __global__ void spmv_tiled_kernel(index_t num_rows, index_t num_cols,
-                                  index_t num_nonzeros, value_t *row_offsets) {
+                                  index_t num_nonzeros, index_t *row_offsets,
+                                  index_t *col_idx, value_t *nonzeros,
+                                  value_t *input, value_t *output) {
+  // Store the output in shared memory
+  extern __shared__ value_t output_shmem[];
   // Each block sets the shared memory region as the output, initialized to 0
   // Iterate up to the tile boundary
 
   // Use Ampere's CUDAMemCpyAsynch
   // Need to use cuda cooperative groups
-
-  // Start with the very basic tiled iteration first, then add fancy Ampere
-  // stuff later
-  if (blockIdx.x == 0 && threadIdx.x == 0) {
-    printf("Num Rows: %d %d %d\n", num_rows, num_cols, num_nonzeros);
-  }
 
   cg::grid_group grid = cg::this_grid();
   grid.sync();
@@ -30,36 +28,70 @@ template <typename index_t = int, typename value_t = float, typename dinput_t,
           typename doutput_t>
 double spmv_tiled(csr_t<index_t, value_t> &A, dinput_t &input,
                   doutput_t &output) {
+  /* ========== Setup Device Properties ========== */
   int device = 0;
-
-  /// This will launch a grid that can maximally fill the GPU, on the default
-  /// stream with kernel arguments
-  int numBlocksPerSm = 0;
-  // Number of threads my_kernel will be launched with
-  int numThreads = 128;
   cudaDeviceProp deviceProp;
   CHECK_CUDA(cudaGetDeviceProperties(&deviceProp, device))
 
-  // Statistics about the GPU device
-  printf(
-      "> GPU device has %d Multi-Processors, SM %d.%d compute capabilities\n\n",
-      deviceProp.multiProcessorCount, deviceProp.major, deviceProp.minor);
+  // Setup grid and block properties
+  int numBlocksPerSm = 0;
+  int numThreadsPerBlock = 0;
+  int shmemPerBlock = 0; // bytes
 
-  int sharedmem = 0;
+  // Use the max number of threads per block to maximize parallelism over shmem
+  numThreadsPerBlock = deviceProp.maxThreadsPerBlock/2;
+
+  // Host code
+  int carveout = 100; // prefer shared memory capacity 100% of maximum
+  // Named Carveout Values:
+  // carveout = cudaSharedmemCarveoutDefault;   //  (-1)
+  // carveout = cudaSharedmemCarveoutMaxL1;     //   (0)
+  // carveout = cudaSharedmemCarveoutMaxShared; // (100)
+
+
+
+  // cudaFuncSetAttribute(spmv_tiled_kernel<int,float>, cudaFuncAttributePreferredSharedMemoryCarveout, carveout);
+
+  shmemPerBlock = deviceProp.sharedMemPerBlockOptin;
+
+  cudaFuncSetAttribute(spmv_tiled_kernel<int,float>, cudaFuncAttributeMaxDynamicSharedMemorySize, shmemPerBlock);
+
+  int rows_per_block = (shmemPerBlock / sizeof(value_t)) / 2;
+  printf("Rows Per Block: %d\n", rows_per_block);
+  printf("Shmem Per Block: %d\n", shmemPerBlock);
+
+  // Need to know the max occupancy to determine how many blocks to launch for
+  // the cooperative kernel. All blocks must be resident on SMs
   CHECK_CUDA(cudaOccupancyMaxActiveBlocksPerMultiprocessor(
-      &numBlocksPerSm, spmv_tiled_kernel<int, float>, numThreads, sharedmem))
-  // launch
+      &numBlocksPerSm, spmv_tiled_kernel<int, float>, numThreadsPerBlock,
+      shmemPerBlock))
+
+  printf("Blocks per SM: %d\n", numBlocksPerSm);
+
+  // CHECK_CUDA(cudaFuncSetAttribute((void *)spmv_tiled_kernel<int,float>,
+  // cudaFuncAttributePreferredSharedMemoryCarveout,
+  // cudaDevAttrMaxSharedMemoryPerMultiprocessor))
+            
+  //                      printf("Max shmem: %d\n", cudaDevAttrMaxSharedMemoryPerBlockOptin);
+
+      /* ========== Setup Kernel Call ========== */
+      void *row_offsets = thrust::raw_pointer_cast(A.d_Ap.data());
+  void *col_idx = thrust::raw_pointer_cast(A.d_Aj.data());
+  void *nonzeros = thrust::raw_pointer_cast(A.d_Ax.data());
   void *input_ptr = thrust::raw_pointer_cast(input.data());
-  void *kernelArgs[] = {(void *)&A.num_rows, (void *)&A.num_columns,
-                        (void *)&A.num_nonzeros, (void*)&input_ptr};
-  dim3 dimBlock(numThreads, 1, 1);
+  void *output_ptr = thrust::raw_pointer_cast(output.data());
+  void *kernelArgs[] = {&A.num_rows,  &A.num_columns, &A.num_nonzeros,
+                        &row_offsets, &col_idx,       &nonzeros,
+                        &input_ptr,   &output_ptr};
+  dim3 dimBlock(numThreadsPerBlock, 1, 1);
   dim3 dimGrid(deviceProp.multiProcessorCount * numBlocksPerSm, 1, 1);
 
-  // execute SpMV
+  /* ========== Execute SPMV ========== */
   Timer t;
   t.start();
-  CHECK_CUDA(cudaLaunchCooperativeKernel(
-      (void *)spmv_tiled_kernel<int, float>, dimGrid, dimBlock, kernelArgs, sharedmem))
+  CHECK_CUDA(cudaLaunchCooperativeKernel((void *)spmv_tiled_kernel<int, float>,
+                                         dimGrid, dimBlock, kernelArgs,
+                                         shmemPerBlock, 0))
 
   CHECK_CUDA(cudaDeviceSynchronize())
   t.stop();
